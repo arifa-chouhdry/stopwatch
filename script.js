12378261d796808bb245dd08d9e5e63e21efa7a2
arifa-chouhdry <48832658+arifa-chouhdry@users.noreply.github.com>
2019-08-22 12:37:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var ss = document.querySelector('.stopwatch');  
+
+[].forEach.call(ss, function (s) {
+    var currentTimer = 0,
+        interval = 0,
+        lastUpdateTime = new Date().getTime(),
+        start = document.querySelector('.start'),
+        stop = document.querySelector('.stop'),
+        reset = document.querySelector('.reset'),
+        mins = document.querySelector('.minutes'),
+        secs = document.querySelector('.seconds'),
+        cents = document.querySelector('.centiseconds');
+
+    start.addEventListener('click', startTimer);
+    stop.addEventListener('click', stopTimer);
+    reset.addEventListener('click', resetTimer);
+
+    function pad (n) {
+        return ('00' + n).substr(-2);
+    }
+
+    function update () {
+        var now = new Date().getTime(),
+            dt = now - lastUpdateTime;
+
+        currentTimer += dt;
+
+        var time = new Date(currentTimer);
+
+        mins.innerHTML = pad(time.getMinutes());
+        secs.innerHTML = pad(time.getSeconds());
+        cents.innerHTML = pad(Math.floor(time.getMilliseconds() / 10));
+
+        lastUpdateTime = now;
+    }
+
+    function startTimer () {
+        if (!interval) {
+            lastUpdateTime = new Date().getTime();
+            interval = setInterval(update, 1);
+        }
+    }
+
+    function stopTimer () {
+        clearInterval(interval);
+        interval = 0;
+    }
+
+    function resetTimer () {
+        stopTimer();
+
+        currentTimer = 0;
+
+        mins.innerHTML = secs.innerHTML = cents.innerHTML = pad(0);
+    }
+});
